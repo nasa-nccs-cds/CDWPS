679640962625025cb9832f36d3dc05e56a99e0e0
Thomas Maxwell <thomas.maxwell@nasa.gov>
2016-01-18 20:55:59 -0500

    Generalize Numbers

--- src/main/scala/nccs/utilities/numbers.scala
+++ src/main/scala/nccs/utilities/numbers.scala
@@ -1,13 +1,26 @@
 package nccs.utilities.numbers
 
+class IllegalNumberException( value: Any ) extends RuntimeException("Error, " + value.toString + " is not a valid Number")
+
 object GenericNumber {
+  val floatingPointNumber = """^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?$""".r
+  val integerNumber = """^[-+]?[0-9]*$""".r
+  def parseString( sx: String ): GenericNumber = {
+    sx.trim match {
+      case  floatingPointNumber(sx) =>  new FloatNumber(sx.toFloat)
+      case  integerNumber(sx) =>        new IntNumber(sx.toInt)
+      case x =>                         throw new IllegalNumberException(x)
+    }
+  }
   def apply( anum: Any = None ): GenericNumber = {
     anum match {
       case ix: Int =>     new IntNumber(ix)
       case fx: Float =>   new FloatNumber(fx)
       case dx: Double =>  new DoubleNumber(dx)
       case sx: Short =>   new ShortNumber(sx)
-      case _ =>           new UndefinedNumber()
+      case None =>        new UndefinedNumber()
+      case sx: String =>  parseString( sx )
+      case x =>           throw new IllegalNumberException(x)
     }
   }
 }
@@ -41,6 +54,11 @@ class ShortNumber( val numvalue: Short ) extends GenericNumber {
 class UndefinedNumber extends GenericNumber {
   type NumericType = Option[Any]
   override def value(): NumericType = { None }
+}
+
+object testNumbers extends App {
+  val vals = List( GenericNumber(40), GenericNumber(40.5), GenericNumber("40"), GenericNumber("40.5") )
+  vals.foreach( x => println( x.toString ) )
 }
 
 
