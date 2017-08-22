d295cc865e2a3d7c2735b5e4bd3aad81bf6c0773
Thomas Maxwell <thomas.maxwell@nasa.gov>
2017-08-22 12:44:13 -0400

    Add unparsed inputs

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -100,7 +100,7 @@ class WPS extends Controller {
             Map("responseform" -> responseform.toString,"storeExecuteResponse" -> storeExecuteResponse.toLowerCase, "async" -> status.toLowerCase)
           logger.info(s"\n\nWPS EXECUTE: identifier=$identifier, service=$service, runargs=$runargs, datainputs=$datainputs\n\n")
           val parsed_data_inputs = wpsObjectParser.parseDataInputs(datainputs)
-          val response: xml.Node = webProcessManager.executeProcess(service, identifier, parsed_data_inputs, runargs)
+          val response: xml.Node = webProcessManager.executeProcess(service, identifier, datainputs, parsed_data_inputs, runargs)
           logger.info("Completed request '%s' in %.4f sec".format(identifier, (System.nanoTime() - t0) / 1.0E9))
           val printer = new scala.xml.PrettyPrinter(200, 3)
           println("---------->>>> Final Result: " + printer.format(response))
