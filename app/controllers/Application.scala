a809e5f9cee6f018e797307c2dbb406cb3be1384
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2017-09-11 14:52:34 -0400

    Update queue

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -18,35 +18,11 @@ import org.apache.commons.lang.RandomStringUtils
 
 class WPS extends Controller {
   val logger = EDASLogManager.getCurrentLogger;  /* LoggerFactory.getLogger("application") */
-  val config: Map[String, String] = serverConfiguration
-  val jobQueue = new PriorityBlockingQueue[Job]()
-  val server_address = config.getOrElse( "edas.server.address", "" )
-  appParameters.setCustomCacheDir( config.getOrElse( "edas.cache.dir", "" ) )
-  appParameters.addConfigParams(config)
 
-  logger.info( "Starting webProcessManager with server_address = " + server_address )
-  val webProcessManager = if( server_address.isEmpty )   { new ProcessManager(config) }
-                          else                           { new zmqProcessManager(config) }
+  val jobQueue = new PriorityBlockingQueue[Job]()
+
   val printer = new scala.xml.PrettyPrinter(200, 3)
 
-  def serverConfiguration: Map[String, String] = {
-    try {
-      val config = Play.current.configuration
-      def get_config_value(key: String): Option[String] = {
-        try {
-          config.getString(key)
-        } catch {
-          case ex: Exception => None
-        }
-      }
-      val map_pairs = for (key <- config.keys; value = get_config_value(key); if value.isDefined)
-        yield (key -> value.get.toString)
-      Map[String, String](map_pairs.toSeq: _*)
-    } catch {
-      case e: Exception =>
-        Map[String, String]("wps.results.dir" -> "~/.wps/results")
-    }
-  }
 
   def getResultFile(id: String, service: String) = Action {
     try {
@@ -90,54 +66,88 @@ class WPS extends Controller {
               status: String,
               datainputs: String) = Action {
     try {
-      request.toLowerCase match {
-        case "getcapabilities" =>
-          logger.info("getcapabilities")
-          print("getcapabilities")
-          Ok(webProcessManager.getCapabilities(service, identifier, Map("syntax"->"WPS"))).withHeaders(ACCESS_CONTROL_ALLOW_ORIGIN -> "*")
-        case "describeprocess" =>
-          logger.info("describeprocess")
-          print("describeprocess")
-          Ok(webProcessManager.describeProcess(service, identifier, Map("syntax"->"WPS"))).withHeaders(ACCESS_CONTROL_ALLOW_ORIGIN -> "*")
-        case "execute" =>
-
-          val runargs = Map("responseform" -> "wps","storeExecuteResponse" -> storeExecuteResponse.toLowerCase, "status" -> status.toLowerCase )
-          logger.info(s"\n\nWPS EXECUTE: server_address=$server_address, identifier=$identifier, service=$service, runargs=$runargs, datainputs=$datainputs\n\n")
-          val parsed_data_inputs = wpsObjectParser.parseDataInputs(datainputs)
-          val rId: String = RandomStringUtils.random( 6, true, true )
-          val request = TaskRequest( rId, service, parsed_data_inputs)
-          val job =  Job( request, identifier, datainputs, runargs )
-          jobQueue.put(job)
-          Ok(response).withHeaders(ACCESS_CONTROL_ALLOW_ORIGIN -> "*")
-      }
-    } catch {
-      case e: BadRequestException =>
-        val error_mesage = CDSecurity.sanitize(  e.getMessage + ":\n" + e.getStackTrace.map( _.toString ).mkString("\n") )
-        BadRequest(
-          <error type="ImproperlyFormedRequest"> {"<![CDATA[\n " + error_mesage + "\n]]>"} </error>).withHeaders(ACCESS_CONTROL_ALLOW_ORIGIN -> "*")
-      case e: NotAcceptableException =>
-        val error_mesage = CDSecurity.sanitize(  e.getMessage + ":\n" + e.getStackTrace.map( _.toString ).mkString("\n") )
-        NotAcceptable(<error type="UnacceptableRequest"> {"<![CDATA[\n " + error_mesage + "\n]]>"} </error>).withHeaders(ACCESS_CONTROL_ALLOW_ORIGIN -> "*")
-      case e: Exception =>
-        val error_mesage = CDSecurity.sanitize(  e.getMessage + ":\n" + e.getStackTrace.map( _.toString ).mkString("\n") )
-        InternalServerError(<error type="InternalServerError"> {"<![CDATA[\n " + error_mesage + "\n]]>"} </error>).withHeaders(ACCESS_CONTROL_ALLOW_ORIGIN -> "*")
+      val runargs = Map("responseform" -> "wps","storeExecuteResponse" -> storeExecuteResponse.toLowerCase, "status" -> status.toLowerCase )
+      logger.info(s"\n\nWPS EXECUTE: server_address=$server_address, identifier=$identifier, service=$service, runargs=$runargs, datainputs=$datainputs\n\n")
+      val parsed_data_inputs = wpsObjectParser.parseDataInputs(datainputs)
+      val rId: String = RandomStringUtils.random( 6, true, true )
+      val request = TaskRequest( rId, service, parsed_data_inputs)
+      val job = Job(request, identifier, datainputs, runargs)
+      jobQueue.put(job)
     }
   }
 }
 
 class serverRequestManager() extends Thread with Loggable {
   override def run() {
+    val config: Map[String, String] = serverConfiguration
+    appParameters.setCustomCacheDir( config.getOrElse( "edas.cache.dir", "" ) )
+    appParameters.addConfigParams(config)
+    val server_address = config.getOrElse( "edas.server.address", "" )
+    logger.info( "Starting webProcessManager with server_address = " + server_address )
+    val webProcessManager = if( server_address.isEmpty )   { new ProcessManager(config) }
+    else                           { new zmqProcessManager(config) }
+
 
   }
 
-  def submitJob( job: Job ): Unit = {
+  def submitJob( processMgr: GenericProcessManager, job: Job ): Unit = {
     val t0 = System.nanoTime()
-    val executionCallback: ExecutionCallback = new ExecutionCallback { override def execute(jobId: String, results: WPSResponse): Unit = {} }
-    val response: xml.Node = webProcessManager.executeProcess( job, Some(executionCallback) )
-    logger.info("Completed request '%s' in %.4f sec".format(job.identifier, (System.nanoTime() - t0) / 1.0E9))
-    val printer = new scala.xml.PrettyPrinter(200, 3)
-    println("---------->>>> Final Result: " + printer.format(response))
+    request.toLowerCase match {
+      case "getcapabilities" =>
+        logger.info("getcapabilities")
+        print("getcapabilities")
+        Ok(webProcessManager.getCapabilities(service, identifier, Map("syntax"->"WPS"))).withHeaders(ACCESS_CONTROL_ALLOW_ORIGIN -> "*")
+      case "describeprocess" =>
+        logger.info("describeprocess")
+        print("describeprocess")
+        Ok(webProcessManager.describeProcess(service, identifier, Map("syntax"->"WPS"))).withHeaders(ACCESS_CONTROL_ALLOW_ORIGIN -> "*")
+      case "execute" =>
+
+        val runargs = Map("responseform" -> "wps","storeExecuteResponse" -> storeExecuteResponse.toLowerCase, "status" -> status.toLowerCase )
+        logger.info(s"\n\nWPS EXECUTE: server_address=$server_address, identifier=$identifier, service=$service, runargs=$runargs, datainputs=$datainputs\n\n")
+        val parsed_data_inputs = wpsObjectParser.parseDataInputs(datainputs)
+        val rId: String = RandomStringUtils.random( 6, true, true )
+        val request = TaskRequest( rId, service, parsed_data_inputs)
+        val executionCallback: ExecutionCallback = new ExecutionCallback { override def execute(jobId: String, results: WPSResponse): Unit = {} }
+        val response: xml.Node = processMgr.executeProcess( job, Some(executionCallback) )
+        logger.info("Completed request '%s' in %.4f sec".format(job.identifier, (System.nanoTime() - t0) / 1.0E9))
+        val printer = new scala.xml.PrettyPrinter(200, 3)
+        println("---------->>>> Final Result: " + printer.format(response))
+        Ok(response).withHeaders(ACCESS_CONTROL_ALLOW_ORIGIN -> "*")
+    }
+  } catch {
+    case e: BadRequestException =>
+      val error_mesage = CDSecurity.sanitize(  e.getMessage + ":\n" + e.getStackTrace.map( _.toString ).mkString("\n") )
+      BadRequest(
+        <error type="ImproperlyFormedRequest"> {"<![CDATA[\n " + error_mesage + "\n]]>"} </error>).withHeaders(ACCESS_CONTROL_ALLOW_ORIGIN -> "*")
+    case e: NotAcceptableException =>
+      val error_mesage = CDSecurity.sanitize(  e.getMessage + ":\n" + e.getStackTrace.map( _.toString ).mkString("\n") )
+      NotAcceptable(<error type="UnacceptableRequest"> {"<![CDATA[\n " + error_mesage + "\n]]>"} </error>).withHeaders(ACCESS_CONTROL_ALLOW_ORIGIN -> "*")
+    case e: Exception =>
+      val error_mesage = CDSecurity.sanitize(  e.getMessage + ":\n" + e.getStackTrace.map( _.toString ).mkString("\n") )
+      InternalServerError(<error type="InternalServerError"> {"<![CDATA[\n " + error_mesage + "\n]]>"} </error>).withHeaders(ACCESS_CONTROL_ALLOW_ORIGIN -> "*")
   }
+  }
+
+  def serverConfiguration: Map[String, String] = {
+    try {
+      val config = Play.current.configuration
+      def get_config_value(key: String): Option[String] = {
+        try {
+          config.getString(key)
+        } catch {
+          case ex: Exception => None
+        }
+      }
+      val map_pairs = for (key <- config.keys; value = get_config_value(key); if value.isDefined)
+        yield (key -> value.get.toString)
+      Map[String, String](map_pairs.toSeq: _*)
+    } catch {
+      case e: Exception =>
+        Map[String, String]("wps.results.dir" -> "~/.wps/results")
+    }
+  }
+
 }
 
 object parseTest extends App {
