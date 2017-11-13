2f3d63f85428e60b6545935f96a4726cc011a11a
Thomas Maxwell <thomas.maxwell@nasa.gov>
2017-11-13 14:52:43 -0500

    Update version  nimbers

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,9 +1,11 @@
 import sbt._
 
+
 object Version {
   val mockito   = "1.10.19"
   val scalaTest = "2.2.4"
   val spark = "1.6.3"
+  val edas = sys.env.getOrElse("EDAS_VERSION","{UNDEFINED}") + "-SNAPSHOT"
 }
 
 object Library {
@@ -12,9 +14,9 @@ object Library {
   val netcdf4        = "edu.ucar"           % "netcdf4"         % "4.6.6"
   val mockitoAll     = "org.mockito"       %  "mockito-all"     % Version.mockito
   val scalaTest      = "org.scalatest"     %% "scalatest"       % Version.scalaTest
-  val edas           = "nasa.nccs"         %% "edas"       % "1.0-SNAPSHOT"
-  val scalactic      = "org.scalactic" %% "scalactic" % "3.0.0"
-  val scalatest      = "org.scalatest" %% "scalatest" % "3.0.0" % "test"
+  val edas           = "nasa.nccs"         %% "edas"            % Version.edas
+  val scalactic      = "org.scalactic"      %% "scalactic"      % "3.0.0"
+  val scalatest      = "org.scalatest"      %% "scalatest"      % "3.0.0" % "test"
   val sparkCore      = "org.apache.spark"  %% "spark-core"      % Version.spark
 }
 
